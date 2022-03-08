7119fb90b8c1cfd60ada658574c207c3f03c0ca8
Md. Forhad Hossain <forhad14@cse.pstu.ac.bd>
2022-03-08 11:26:37 +0600

    setting up home page

---
+++ Blog_in_Django/urls.py
@@ -0,0 +1,17 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('home.urls')),
+    path('froala_editor/', include('froala_editor.urls'))
+]
+
+# Static file: Image, media and others handling
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
+urlpatterns += staticfiles_urlpatterns()
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Blog_in_Django
  Simple blog. in Django
+
+https://www.w3schools.com/w3css/tryit.asp?filename=tryw3css_templates_blog&stacked=h
